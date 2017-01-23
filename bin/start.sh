3bb3a47e6b2c9be71e410fc6afa1f764008ce3b6
宋永忠 <songyongzhong@le.com>
2017-01-23 11:23:23 +0800

    start script

--- bin/start.sh
+++ bin/start.sh
@@ -1,30 +1,4 @@
 #!/bin/sh
-if [ -z "$ROCKETMQ_CONSOLE_HOME" ] ; then
-  ## resolve links - $0 may be a link to maven's home
-  PRG="$0"
-
-  # need this for relative symlinks
-  while [ -h "$PRG" ] ; do
-    ls=`ls -ld "$PRG"`
-    link=`expr "$ls" : '.*-> \(.*\)$'`
-    if expr "$link" : '/.*' > /dev/null; then
-      PRG="$link"
-    else
-      PRG="`dirname "$PRG"`/$link"
-    fi
-  done
-
-  saveddir=`pwd`
-
-  ROCKETMQ_CONSOLE_HOME=`dirname "$PRG"`/..
-
-  # make it fully qualified
-  ROCKETMQ_CONSOLE_HOME=`cd "$ROCKETMQ_CONSOLE_HOME" && pwd`
-
-  cd "$saveddir"
-fi
-
-export ROCKETMQ_CONSOLE_HOME
-
-sh ${ROCKETMQ_CONSOLE_HOME}/bin/runserver.sh org.apache.rocketmq.console.App $@
+export JAVA="/root/jdk1.7.0_76/bin/java"
+$JAVA -jar /root/app2/rocketmq-console-ng/target/rocketmq-console-1.0.0-SNAPSHOT.jar
 
