f97e9e6412ef3df3a57e8b9cbdda770a1e9723cb
zhaoqun007 <911@zhaoqun911.cn>
2017-05-25 14:53:23 +0800

    1.0-SNAPSHOT version

--- rocketmq-mysql/README.md
+++ rocketmq-mysql/README.md
@@ -4,12 +4,64 @@
 ## Overview
 ![overview](./doc/overview.png)
 
-The RocketMQ-MySQL is a data replicator between MySQL and other system,the replicator parse the binglog event and send it in json format to the RocketMQ,and other system can pull data from RocketMQ.
+The RocketMQ-MySQL is a data replicator between MySQL and other systems, 
+the replicator simulates a MySQL slave instance and parses the binlog event 
+and sends it to the RocketMQ in json format ,other systems can consume data from RocketMQ. 
+With the RocketMQ-MySQL Replicator,more systems can process data from MySQL binlog 
+in a simple and low cost method.
+
 ## Dataflow
 ![dataflow](./doc/dataflow.png)
 
-* 1.Firstly,get the last data from the queue,and get the binglog position from this data,and if the queue data is null,then use the latest binglog position of the MySQL,and surely user can also specify this position on his own;
+* 1.Firstly,get the last data from the queue,and get the binlog position from this data,and if the queue data is null,then use the latest binlog position of the MySQL,and surely user can also specify this position on his own;
 * 2.Send a binlog dump request to the MySQL;
-* 3.The MySQL push binglog event to the Replicator,the Replicator parse the data and accumulate as a transaction-object;
+* 3.The MySQL push binlog event to the replicator,the replicator parses the data and accumulate as a transaction-object;
 * 4.Add the next-position of the transaction to the transaction-object and send it in json format to the queue;
-* 5.Record the binglog position and the offset in the queue of the latest transaction every second.
+* 5.Record the binlog position and the offset in the queue of the latest transaction every second.
+
+
+## Quick Start
+
+* 1.Create an account with MySQL replication permission,which is used to simulate the MySQL slave to get the binlog event,and the replication must be in row mode;
+* 2.Create a topic in the RocketMQ to store binlog events,in order to ensure that the downstream system consumes the data orderly,the topic must have only one queue;
+* 3.Configure relevant information of MySQL and RocketMQ in the RocketMQ-MySQL.conf file;
+* 4.Execute"mvn install",and then start the replicator(execute "nohup ./start.sh &");
+* 5.Subscribe to and process the messages in your system.
+
+
+## Configuration Instruction
+|key               |nullable|default    |description|
+|------------------|--------|-----------|-----------|
+|mysqlAddr         |false   |           |MySQL address|
+|mysqlPort         |false   |           |MySQL port|
+|mysqlUsername     |false   |           |username of MySQL account|
+|mysqlPassword     |false   |           |password of MySQL account|
+|mqNamesrvAddr     |false   |           |RocketMQ name server address (e.g.,127.0.0.1:9876)|
+|mqTopic           |false   |           |RocketMQ topic name|
+|startType         |true    |NEW_EVENT  |The way that the replicator starts processing data,there are three options available:<br>- NEW_EVENT： starts processing data from the tail of binlog<br>- LAST_PROCESSED: starts processing data from the last processed event<br>- SPECIFIED:starts processing data from the position that user specified,if you choose this option,the binlogFilename and nextPosition must not be null|
+|binlogFilename    |true    |           |If "startType" is "SPECIFIED",the replicator will begin to replicate from this binlog file|
+|nextPosition      |true    |           |If "startType" is "SPECIFIED",the replicator will begin to replicate from this position|
+|maxTransactionRows|true    |100        |max rows of the transaction pushed to RocketMQ|
+
+## Best Practice
+
+### Subscribe to new data only(simple)
+If your system only wants to subscribe to MySQL's new data and don't want to obtain the past data that was already exists,
+you can use the method described in "Quick Start".
+
+### Migrate complete data and subscribe to new data
+If you want to migrate complete data from MySQL to other system and continuously subscribe to new data,
+you can use the following method:
+* 1.Start the replicator first by following the method described in "Quick Start"(1~4),
+and then choose a method you think is appropriate to migrate existing data from MySQL to other system,
+and you need to write down the current time before migrating the data.
+* 2.After the data migration is completed,open the offset log file,
+according to the previously recorded migration start time select an offset corresponding to a time point,
+this time point needs be earlier than the initial migration time and as close as possible to the current time.<br>
+* 3.Now,with this offset you can continuously pull data from the queue and process it in your system.
+
+## About The Offset Log
+Replicator will record the nextPosition of the latest transaction and the nextOffset of the queue every second to the offset.log,
+and if you want to start subscribing to event from a time point or a binlog position,
+you can find the corresponding offset through offset log,and then pull data from RocketMQ through this offset.
+
---
+++ rocketmq-mysql/src/main/java/org/apache/rocketmq/mysql/binlog/EventProcessor.java
@@ -0,0 +1,321 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.rocketmq.mysql.binlog;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.google.code.or.OpenReplicator;
+import com.google.code.or.binlog.BinlogEventV4;
+import com.google.code.or.binlog.impl.event.DeleteRowsEvent;
+import com.google.code.or.binlog.impl.event.DeleteRowsEventV2;
+import com.google.code.or.binlog.impl.event.QueryEvent;
+import com.google.code.or.binlog.impl.event.TableMapEvent;
+import com.google.code.or.binlog.impl.event.UpdateRowsEvent;
+import com.google.code.or.binlog.impl.event.UpdateRowsEventV2;
+import com.google.code.or.binlog.impl.event.WriteRowsEvent;
+import com.google.code.or.binlog.impl.event.WriteRowsEventV2;
+import com.google.code.or.binlog.impl.event.XidEvent;
+import com.google.code.or.common.glossary.Pair;
+import com.google.code.or.common.glossary.Row;
+import com.google.code.or.common.util.MySQLConstants;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.regex.Pattern;
+import javax.sql.DataSource;
+import org.apache.rocketmq.mysql.Config;
+import org.apache.rocketmq.mysql.Replicator;
+import org.apache.rocketmq.mysql.position.BinlogPosition;
+import org.apache.rocketmq.mysql.position.BinlogPositionManager;
+import org.apache.rocketmq.mysql.schema.Schema;
+import org.apache.rocketmq.mysql.schema.Table;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ */
+public class EventProcessor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventProcessor.class);
+
+    private Replicator replicator;
+    private Config config;
+
+    private DataSource dataSource;
+
+    private BinlogPositionManager binlogPositionManager;
+
+    private BlockingQueue<BinlogEventV4> queue = new LinkedBlockingQueue<>(100);
+
+    private OpenReplicator openReplicator;
+
+    private EventListener eventListener;
+
+    private Schema schema;
+
+    private Map<Long, Table> tableMap = new HashMap<>();
+
+    private Transaction transaction;
+
+    public EventProcessor(Replicator replicator) {
+
+        this.replicator = replicator;
+        this.config = replicator.getConfig();
+    }
+
+    public void start() throws Exception {
+
+        initDataSource();
+
+        binlogPositionManager = new BinlogPositionManager(config, dataSource);
+        binlogPositionManager.initBeginPosition();
+
+        schema = new Schema(dataSource);
+        schema.load();
+
+        eventListener = new EventListener(queue);
+        openReplicator = new OpenReplicator();
+        openReplicator.setBinlogEventListener(eventListener);
+        openReplicator.setHost(config.mysqlAddr);
+        openReplicator.setPort(config.mysqlPort);
+        openReplicator.setUser(config.mysqlUsername);
+        openReplicator.setPassword(config.mysqlPassword);
+        openReplicator.setStopOnEOF(false);
+        openReplicator.setHeartbeatPeriod(1f);
+        openReplicator.setLevel2BufferSize(50 * 1024 * 1024);
+        openReplicator.setServerId(6379);
+        openReplicator.setBinlogFileName(binlogPositionManager.getBinlogFilename());
+        openReplicator.setBinlogPosition(binlogPositionManager.getPosition());
+        openReplicator.start();
+
+        LOGGER.info("Started.");
+
+        doProcess();
+    }
+
+    private void doProcess() {
+
+        while (true) {
+
+            try {
+                BinlogEventV4 event = queue.take();
+
+                switch (event.getHeader().getEventType()) {
+                    case MySQLConstants.TABLE_MAP_EVENT:
+                        processTableMapEvent(event);
+                        break;
+
+                    case MySQLConstants.WRITE_ROWS_EVENT:
+                        processWriteEvent(event);
+                        break;
+
+                    case MySQLConstants.WRITE_ROWS_EVENT_V2:
+                        processWriteEventV2(event);
+                        break;
+
+                    case MySQLConstants.UPDATE_ROWS_EVENT:
+                        processUpdateEvent(event);
+                        break;
+
+                    case MySQLConstants.UPDATE_ROWS_EVENT_V2:
+                        processUpdateEventV2(event);
+                        break;
+
+                    case MySQLConstants.DELETE_ROWS_EVENT:
+                        processDeleteEvent(event);
+                        break;
+
+                    case MySQLConstants.DELETE_ROWS_EVENT_V2:
+                        processDeleteEventV2(event);
+                        break;
+
+                    case MySQLConstants.QUERY_EVENT:
+                        processQueryEvent(event);
+                        break;
+
+                    case MySQLConstants.XID_EVENT:
+                        processXidEvent(event);
+                        break;
+
+                }
+            } catch (Exception e) {
+                LOGGER.error("Binlog process error.", e);
+            }
+
+        }
+    }
+
+    private void processTableMapEvent(BinlogEventV4 event) {
+
+        TableMapEvent tableMapEvent = (TableMapEvent) event;
+        String dbName = tableMapEvent.getDatabaseName().toString();
+        String tableName = tableMapEvent.getTableName().toString();
+        Long tableId = tableMapEvent.getTableId();
+
+        Table table = schema.getTable(dbName, tableName);
+
+        tableMap.put(tableId, table);
+    }
+
+    private void processWriteEvent(BinlogEventV4 event) {
+
+        WriteRowsEvent writeRowsEvent = (WriteRowsEvent) event;
+        Long tableId = writeRowsEvent.getTableId();
+        List<Row> list = writeRowsEvent.getRows();
+
+        for (Row row : list) {
+            addRow("WRITE", tableId, row);
+        }
+    }
+
+    private void processWriteEventV2(BinlogEventV4 event) {
+
+        WriteRowsEventV2 writeRowsEventV2 = (WriteRowsEventV2) event;
+        Long tableId = writeRowsEventV2.getTableId();
+        List<Row> list = writeRowsEventV2.getRows();
+
+        for (Row row : list) {
+            addRow("WRITE", tableId, row);
+        }
+
+    }
+
+    private void processUpdateEvent(BinlogEventV4 event) {
+
+        UpdateRowsEvent updateRowsEvent = (UpdateRowsEvent) event;
+        Long tableId = updateRowsEvent.getTableId();
+        List<Pair<Row>> list = updateRowsEvent.getRows();
+
+        for (Pair<Row> pair : list) {
+            addRow("UPDATE", tableId, pair.getAfter());
+        }
+    }
+
+    private void processUpdateEventV2(BinlogEventV4 event) {
+
+        UpdateRowsEventV2 updateRowsEventV2 = (UpdateRowsEventV2) event;
+        Long tableId = updateRowsEventV2.getTableId();
+        List<Pair<Row>> list = updateRowsEventV2.getRows();
+
+        for (Pair<Row> pair : list) {
+            addRow("UPDATE", tableId, pair.getAfter());
+        }
+    }
+
+    private void processDeleteEvent(BinlogEventV4 event) {
+
+        DeleteRowsEvent deleteRowsEvent = (DeleteRowsEvent) event;
+        Long tableId = deleteRowsEvent.getTableId();
+        List<Row> list = deleteRowsEvent.getRows();
+
+        for (Row row : list) {
+            addRow("DELETE", tableId, row);
+        }
+
+    }
+
+    private void processDeleteEventV2(BinlogEventV4 event) {
+
+        DeleteRowsEventV2 deleteRowsEventV2 = (DeleteRowsEventV2) event;
+        Long tableId = deleteRowsEventV2.getTableId();
+        List<Row> list = deleteRowsEventV2.getRows();
+
+        for (Row row : list) {
+            addRow("DELETE", tableId, row);
+        }
+
+    }
+
+    private static Pattern createTablePattern =
+        Pattern.compile("^(CREATE|ALTER)\\s+TABLE", Pattern.CASE_INSENSITIVE);
+
+    private void processQueryEvent(BinlogEventV4 event) {
+
+        QueryEvent queryEvent = (QueryEvent) event;
+        String sql = queryEvent.getSql().toString();
+
+        if (createTablePattern.matcher(sql).find()) {
+            schema.reset();
+        }
+    }
+
+    private void processXidEvent(BinlogEventV4 event) {
+
+        XidEvent xidEvent = (XidEvent) event;
+        String binlogFilename = xidEvent.getBinlogFilename();
+        Long position = xidEvent.getHeader().getNextPosition();
+        Long xid = xidEvent.getXid();
+
+        BinlogPosition binlogPosition = new BinlogPosition(binlogFilename, position);
+        transaction.setNextBinlogPosition(binlogPosition);
+        transaction.setXid(xid);
+
+        replicator.commit(transaction, true);
+
+        transaction = new Transaction(this);
+
+    }
+
+    private void addRow(String type, Long tableId, Row row) {
+
+        if (transaction == null) {
+            transaction = new Transaction(this);
+        }
+
+        Table t = tableMap.get(tableId);
+        if (t != null) {
+
+            while (true) {
+                if (transaction.addRow(type, t, row)) {
+                    break;
+
+                } else {
+                    transaction.setNextBinlogPosition(replicator.getNextBinlogPosition());
+                    replicator.commit(transaction, false);
+                    transaction = new Transaction(this);
+                }
+            }
+
+        }
+    }
+
+    private void initDataSource() throws Exception {
+
+        Map map = new HashMap();
+        map.put("driverClassName", "com.mysql.jdbc.Driver");
+        map.put("url", "jdbc:mysql://" + config.mysqlAddr + ":" + config.mysqlPort + "?useSSL=true&verifyServerCertificate=false");
+        map.put("username", config.mysqlUsername);
+        map.put("password", config.mysqlPassword);
+        map.put("initialSize", "2");
+        map.put("maxActive", "2");
+        map.put("maxWait", "60000");
+        map.put("timeBetweenEvictionRunsMillis", "60000");
+        map.put("minEvictableIdleTimeMillis", "300000");
+        map.put("validationQuery", "SELECT 1 FROM DUAL");
+        map.put("testWhileIdle", "true");
+
+        dataSource = DruidDataSourceFactory.createDataSource(map);
+    }
+
+    public Config getConfig() {
+        return config;
+    }
+
+}
