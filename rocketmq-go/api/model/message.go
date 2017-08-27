82d26055a56e672a68c6f991fc2da3b8f4b3505e
StyleTang <styletang.me@gmail.com>
2017-08-27 18:17:38 +0800

    rocketmq api

--- rocketmq-go/api/model/message.go
+++ rocketmq-go/api/model/message.go
@@ -1,3 +1,20 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package rocketmqm
 
 import "github.com/apache/incubator-rocketmq-externals/rocketmq-go/model/message"
@@ -9,21 +26,21 @@ import "github.com/apache/incubator-rocketmq-externals/rocketmq-go/model/message
 // body: the message's user content
 // see MessageImpl
 type Message interface {
-	//Topic get topic
+	//Topic Topic get topic
 	Topic() (topic string)
-	//set message topic
+	//SetTopic set message topic
 	SetTopic(tag string)
 
-	//set message tag
+	//SetTag set message tag
 	SetTag(tag string)
-	//get message tag
+	//Tag get message tag
 	Tag() (tag string)
-	//set message key
+	//SetKeys set message key
 	SetKeys(keys []string)
 
-	// set delay time level
+	//SetDelayTimeLevel set delay time level
 	SetDelayTimeLevel(delayTimeLevel int)
-	// set body
+	//SetBody set body
 	SetBody([]byte)
 }
 
