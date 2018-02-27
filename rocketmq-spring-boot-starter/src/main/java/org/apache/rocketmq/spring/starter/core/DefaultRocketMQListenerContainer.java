9e1917d16974b2a78e87b2f8f07978a25c7fe009
liweitang <liweitang@xingfeiinc.com>
2018-02-27 15:08:54 +0800

    fix bug for: if rocketMQListener is a AopProxy, and the messageType will be wrong

--- rocketmq-spring-boot-starter/src/main/java/org/apache/rocketmq/spring/starter/core/DefaultRocketMQListenerContainer.java
+++ rocketmq-spring-boot-starter/src/main/java/org/apache/rocketmq/spring/starter/core/DefaultRocketMQListenerContainer.java
@@ -40,6 +40,7 @@ import org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.Assert;
 
@@ -225,7 +226,13 @@ public class DefaultRocketMQListenerContainer implements InitializingBean, Rocke
 
     private JavaType getMessageType() {
         Type messageType = Object.class;
-        Type[] interfaces = rocketMQListener.getClass().getGenericInterfaces();
+        Type[] interfaces = null;
+        if (AopUtils.isAopProxy(rocketMQListener)) {
+            interfaces = AopUtils.getTargetClass(rocketMQListener).getGenericInterfaces();
+        } else {
+            interfaces = rocketMQListener.getClass().getGenericInterfaces();
+        }
+
         if (Objects.nonNull(interfaces)) {
             for (Type type : interfaces) {
                 if (type instanceof ParameterizedType) {
