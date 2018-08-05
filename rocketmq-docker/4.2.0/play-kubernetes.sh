8090e866e938a97e9fcbbb03c4c78353bb2e1b46
huanwei <huan@harmonycloud.cn>
2018-08-05 16:06:00 +0800

    remome private docker repository example
    Signed-off-by: huanwei <huan@harmonycloud.cn>

--- rocketmq-docker/4.2.0/play-kubernetes.sh
+++ rocketmq-docker/4.2.0/play-kubernetes.sh
@@ -7,13 +7,5 @@ docker build -t apache/rocketmq-base:4.2.0 --build-arg version=4.2.0 ./rocketmq-
 docker build -t apache/rocketmq-namesrv:4.2.0-k8s ./kubernetes/rocketmq-namesrv
 docker build -t apache/rocketmq-broker:4.2.0-k8s ./kubernetes/rocketmq-broker
 
-# Then `docker tag` images, and `docker push` images to respective registry
-# For example:
-#docker tag apache/rocketmq-namesrv:4.2.0-k8s huanwei/rocketmq-namesrv:4.2.0-k8s
-#docker tag apache/rocketmq-broker:4.2.0-k8s huanwei/rocketmq-broker:4.2.0-k8s
-#docker push huanwei/rocketmq-namesrv:4.2.0-k8s
-#docker push huanwei/rocketmq-broker:4.2.0-k8s
-
-# Finally, run namesrv and broker on your Kubernetes cluster.
-# For example:
-#kubectl create -f kubernetes/deployment.yaml
+# Run namesrv and broker on your Kubernetes cluster.
+kubectl create -f kubernetes/deployment.yaml
