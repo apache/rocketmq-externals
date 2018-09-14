753f624239ba3bbd25ddff7e862dd501dc8dc5c3
xiaozy <xiaozy@fenbi.com>
2018-09-14 21:07:18 +0800

    FIX license error

--- iot-bridge/bin/sample-producer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CURDIR=`cd $(dirname $0) && pwd`
-BASEDIR=$(dirname ${CURDIR})
-MAINCLASS=org.apache.rocketmq.iot.example.MqttSampleProducer
-VERSION=0.0.1-SNAPSHOT
-JAR=rocketmq-iot-bridge-${VERSION}.jar
-java -cp "${BASEDIR}/target/${JAR}" ${MAINCLASS}
--- iot-bridge/src/main/java/org/apache/rocketmq/iot/common/configuration/ChannelConfiguration.java
+++
@@ -1,8 +0,0 @@
-package org.apache.rocketmq.iot.common.configuration;
-
-import io.netty.util.AttributeKey;
-
-public class ChannelConfiguration {
-    public static final String CHANNEL_IDLE_TIME = "channelIdleTime";
-    public static final AttributeKey<Integer> CHANNEL_IDLE_TIME_ATTRIBUTE_KEY = AttributeKey.valueOf(CHANNEL_IDLE_TIME);
-}
