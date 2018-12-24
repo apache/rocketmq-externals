82da099aaeed1eb282706cc57e109d04f519b3ee
huanwei <huan@harmonycloud.cn>
2018-12-25 00:15:58 +0800

    Docker refactor for RocketMQ 4.2.0
    Signed-off-by: huanwei <huan@harmonycloud.cn>

--- rocketmq-docker/4.2.0/play-docker.sh
+++ rocketmq-docker/4.2.0/play-docker.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Build base image
-docker build -t apache/rocketmq-base:4.2.0 --build-arg version=4.2.0 ./rocketmq-base
-
-# Build namesrv and broker
-docker build -t apache/rocketmq-namesrv:4.2.0 ./rocketmq-namesrv
-docker build -t apache/rocketmq-broker:4.2.0 ./rocketmq-broker
+# Build rocketmq
+docker build -t apache/rocketmq:4.2.0 --build-arg version=4.2.0 ./rocketmq
 
 # Run namesrv and broker
-
-docker run -d -p 9876:9876 --name rmqnamesrv  apache/rocketmq-namesrv:4.2.0
-docker run -d -p 10911:10911 -p 10909:10909 --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" apache/rocketmq-broker:4.2.0
+docker run -d -p 9876:9876 -v `pwd`/data/namesrv/logs:/root/logs -v `pwd`/data/namesrv/store:/root/store --name rmqnamesrv  apache/rocketmq:4.2.0 sh mqnamesrv
+docker run -d -p 10911:10911 -p 10909:10909 -v `pwd`/data/broker/logs:/root/logs -v `pwd`/data/broker/store:/root/store --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" apache/rocketmq:4.2.0 sh mqbroker
