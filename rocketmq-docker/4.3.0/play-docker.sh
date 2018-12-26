26631cbd8915bd900570832e29a3b1d4b185e3f7
huanwei <huan@harmonycloud.cn>
2018-12-26 22:10:22 +0800

    update the play scripts, to remove the existing RocketMQ containers before running the new containers
    Signed-off-by: huanwei <huan@harmonycloud.cn>

--- rocketmq-docker/4.3.0/play-docker.sh
+++ rocketmq-docker/4.3.0/play-docker.sh
@@ -3,6 +3,9 @@
 # Build rocketmq
 docker build -t apache/rocketmq:4.3.0 --build-arg version=4.3.0 ./rocketmq
 
+# Stop and remove existed containers if any
+docker rm -f $(docker ps -a|awk '/apache\/rocketmq/ {print $1}')
+
 # Run namesrv and broker
 docker run -d -p 9876:9876 -v `pwd`/data/namesrv/logs:/root/logs -v `pwd`/data/namesrv/store:/root/store --name rmqnamesrv  apache/rocketmq:4.3.0 sh mqnamesrv
 docker run -d -p 10911:10911 -p 10909:10909 -v `pwd`/data/broker/logs:/root/logs -v `pwd`/data/broker/store:/root/store --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" apache/rocketmq:4.3.0 sh mqbroker
