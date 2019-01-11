025aca3a5050c071184830adbf0f5644f30d9e1d
huanwei <huan@harmonycloud.cn>
2019-01-11 12:47:05 +0800

    use temporary docker images repository
    Signed-off-by: huanwei <huan@harmonycloud.cn>

--- rocketmq-docker/4.3.1/play-docker.sh
+++ rocketmq-docker/4.3.1/play-docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Build rocketmq
-docker build -t apache/rocketmq:4.3.1 --build-arg version=4.3.1 ./rocketmq
+docker build -t huanwei/rocketmq:4.3.1 --build-arg version=4.3.1 ./rocketmq
 
 # Stop and remove existed containers if any
-docker rm -f $(docker ps -a|awk '/apache\/rocketmq/ {print $1}')
+docker rm -f $(docker ps -a|awk '/huanwei\/rocketmq/ {print $1}')
 
 # Run namesrv and broker
-docker run -d -p 9876:9876 -v `pwd`/data/namesrv/logs:/root/logs -v `pwd`/data/namesrv/store:/root/store --name rmqnamesrv  apache/rocketmq:4.3.1 sh mqnamesrv
-docker run -d -p 10911:10911 -p 10909:10909 -v `pwd`/data/broker/logs:/root/logs -v `pwd`/data/broker/store:/root/store --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" apache/rocketmq:4.3.1 sh mqbroker
+docker run -d -p 9876:9876 -v `pwd`/data/namesrv/logs:/root/logs -v `pwd`/data/namesrv/store:/root/store --name rmqnamesrv  huanwei/rocketmq:4.3.1 sh mqnamesrv
+docker run -d -p 10911:10911 -p 10909:10909 -v `pwd`/data/broker/logs:/root/logs -v `pwd`/data/broker/store:/root/store --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" huanwei/rocketmq:4.3.1 sh mqbroker
