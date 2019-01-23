2702d3b7ee2679be52720e13564c64b5dfcfddf1
Harrycoda Wang <harrycodawang@gmail.com>
2019-01-23 22:48:32 +0800

    Add README for Dockerfile of every version

--- rocketmq-docker/4.3.1/play-docker.sh
+++ rocketmq-docker/4.3.1/play-docker.sh
@@ -6,3 +6,6 @@ docker rm -f $(docker ps -a|awk '/rocketmqinc\/rocketmq/ {print $1}')
 # Run namesrv and broker
 docker run -d -p 9876:9876 -v `pwd`/data/namesrv/logs:/root/logs -v `pwd`/data/namesrv/store:/root/store --name rmqnamesrv  rocketmqinc/rocketmq:4.3.1 sh mqnamesrv
 docker run -d -p 10911:10911 -p 10909:10909 -v `pwd`/data/broker/logs:/root/logs -v `pwd`/data/broker/store:/root/store --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" rocketmqinc/rocketmq:4.3.1 sh mqbroker
+
+# Test the cluster list
+docker exec -ti rmqbroker ./mqadmin clusterList
--- rocketmq-docker/4.4.0/build-image.sh
+++ rocketmq-docker/4.4.0/build-image.sh
@@ -4,14 +4,5 @@ then
   ROCKETMQ_VERSION="4.4.0"
 fi
 
-# Save current dir
-CURRENT_DIR="$(dirname $0)"
-
-# Change to dir of Dockerfile
-pushd ${CURRENT_DIR}
-
 # Build rocketmq
 docker build -t rocketmqinc/rocketmq:${ROCKETMQ_VERSION} --build-arg version=${ROCKETMQ_VERSION} .
-
-# Change back to current dir
-popd
