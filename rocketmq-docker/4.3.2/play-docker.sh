c991cc50d9b7e7c4e01678bf8e2ad561d5f72aa4
Harrycoda Wang <harrycodawang@gmail.com>
2019-01-24 17:53:21 +0800

    Add wait time for remove existing container

--- rocketmq-docker/4.3.2/play-docker.sh
+++ rocketmq-docker/4.3.2/play-docker.sh
@@ -3,6 +3,9 @@
 # Stop and remove existed containers if any
 docker rm -f $(docker ps -a|awk '/rocketmqinc\/rocketmq/ {print $1}')
 
+# Wait till the existing containers are removed
+sleep 5
+
 # Run namesrv and broker
 docker run -d -p 9876:9876 -v `pwd`/data/namesrv/logs:/root/logs -v `pwd`/data/namesrv/store:/root/store --name rmqnamesrv  rocketmqinc/rocketmq:4.3.2 sh mqnamesrv
 docker run -d -p 10911:10911 -p 10909:10909 -v `pwd`/data/broker/logs:/root/logs -v `pwd`/data/broker/store:/root/store --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" rocketmqinc/rocketmq:4.3.2 sh mqbroker
