9576aa9a87573324ca9006ed2472568b5474d94f
wlliqipeng <qipengli.lqp@alibaba-inc.com>
2019-02-23 16:54:54 +0800

    Add a consumer example to donet

---
+++ rocketmq-client-donet/example/consumer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using RocketMQ.Interop;
+
+namespace rocketmq_push_consumer_test
+{
+    class Program
+    {
+        private static readonly PushConsumerWrap.MessageCallBack callback = new PushConsumerWrap.MessageCallBack(HandleMessageCallBack);
+
+        static void Main(string[] args)
+        {
+            //Task.Run(() => {
+            Console.WriteLine("start push consumer...");
+
+            var consumer = PushConsumerWrap.CreatePushConsumer("xxx");
+            Console.WriteLine($"consumer: {consumer}");
+            var r0 = PushConsumerWrap.SetPushConsumerLogLevel(consumer, CLogLevel.E_LOG_LEVEL_TRACE);
+
+            var groupId = PushConsumerWrap.GetPushConsumerGroupID(consumer);
+            Console.WriteLine($"groupId: {groupId}");
+
+            var r1 = PushConsumerWrap.SetPushConsumerNameServerAddress(consumer, "47.101.55.250:9876");
+            var r2 = PushConsumerWrap.Subscribe(consumer, "test", "*");
+            var r3 = PushConsumerWrap.RegisterMessageCallback(consumer, callback);
+            var r10 = PushConsumerWrap.StartPushConsumer(consumer);
+            Console.WriteLine($"start push consumer ptr: {r10}");
+
+            while (true)
+            {
+                Thread.Sleep(500);
+            }
+            //});
+            Console.ReadKey(true);
+
+            //PushConsumerBinder.DestroyPushConsumer(consumer);
+        }
+
+        public static int HandleMessageCallBack(IntPtr consumer, IntPtr message)
+        {
+            Console.WriteLine($"consumer: {consumer}; messagePtr: {message}");
+
+            var body = MessageWrap.GetMessageBody(message);
+            var messageId = MessageWrap.GetMessageId(message);
+            Console.WriteLine($"body: {body}");
+
+            return 0;
+        }
+    }
+}
---
+++ rocketmq-client-donet/src/MessageWrap.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RocketMQ.Interop
+{
+    public static class MessageWrap
+    {
+        [DllImport(ConstValues.RocketMQDriverDllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr CreateMessage(string topic);
+
+        [DllImport(ConstValues.RocketMQDriverDllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SetMessageTags(IntPtr messageIntPtr, string tags);
+
+        [DllImport(ConstValues.RocketMQDriverDllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int SetMessageBody(IntPtr messageIntPtr, string body);
+        
+        [DllImport(ConstValues.RocketMQDriverDllName, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPTStr)]
+        public static extern string GetMessageTopic(IntPtr message);
+
+        [DllImport(ConstValues.RocketMQDriverDllName, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPTStr)]
+        public static extern string GetMessageTags(IntPtr message);
+
+        [DllImport(ConstValues.RocketMQDriverDllName, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPTStr)]
+        public static extern string GetMessageKeys(IntPtr message);
+
+        [DllImport(ConstValues.RocketMQDriverDllName, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern string GetMessageBody(IntPtr message);
+
+        [DllImport(ConstValues.RocketMQDriverDllName, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern string GetMessageProperty(IntPtr message, string key);
+
+        [DllImport(ConstValues.RocketMQDriverDllName, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern string GetMessageId(IntPtr message);
+    }
+}
