e0de9edfc3b3f3e328486ab8f89ac978fd821046
vongosling <vongosling@apache.org>
2019-04-19 15:38:56 +0800

    Make shell more robustness

--- rocketmq-docker/4.4.0/play-docker.sh
+++ rocketmq-docker/4.4.0/play-docker.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-# Stop and remove existed containers if any
-docker rm -fv $(docker ps -a|awk '/rmq/ {print $1}')
-
-# Wait till the existing containers are removed
-sleep 5
+RMQ_CONTAINER=$(docker ps -a|awk '/rmq/ {print $1}')
+if [[ -n "$RMQ_CONTAINER" ]]; then
+   echo "Removing RocketMQ Container..."
+   docker rm -fv $RMQ_CONTAINER
+   # Wait till the existing containers are removed
+   sleep 5
+fi
 
 if [ ! -d "`pwd`/data" ]; then
   mkdir -p "data"
 fi
 
-chown -R 3000:3000 data
+echo "Starting RocketMQ nodes..."
 
-# Run namesrv and broker
+# Start nameserver
 docker run -d -p 9876:9876 -v `pwd`/data/namesrv/logs:/home/rocketmq/logs -v `pwd`/data/namesrv/store:/home/rocketmq/store --name rmqnamesrv  rocketmqinc/rocketmq:4.4.0 sh mqnamesrv
+
+# Start Broker
 docker run -d -p 10911:10911 -p 10909:10909 -v `pwd`/data/broker/logs:/home/rocketmq/logs -v `pwd`/data/broker/store:/home/rocketmq/store --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" rocketmqinc/rocketmq:4.4.0 sh mqbroker
+
+# Servive unavailable when not ready
+sleep 10
 
 # Test to produce messages
 sh ./play-producer.sh
