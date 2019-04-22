6b7728c6d06c64decb1eaf065d3f928fa607520d
vongosling <vongosling@apache.org>
2019-04-22 11:25:51 +0800

    Support idempotence deployment script

--- rocketmq-docker/4.4.0/play-kubernetes.sh
+++ rocketmq-docker/4.4.0/play-kubernetes.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+RMQ_DEPLOYMENT=$(kubectl get deployments|awk '/rocketmq-k8s/ {print $1}')
+if [[ -n "$RMQ_DEPLOYMENT" ]]; then
+   kubectl delete -f kubernetes/deployment.yaml
+   sleep 3
+fi
+
 # Run namesrv and broker on your Kubernetes cluster
 kubectl create -f kubernetes/deployment.yaml
