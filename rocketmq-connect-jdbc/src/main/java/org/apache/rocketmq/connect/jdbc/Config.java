f1a802093c8aa5c4de4910f02dbd934599c1d2c1
yuchenlichuck <yuchenlichuck@126.com>
2019-07-18 22:52:55 +0800

    Add Jdbc Source Connector and Do Unit Test

---
+++ rocketmq-connect-jdbc/src/main/java/org/apache/rocketmq/connect/jdbc/Config.java
@@ -0,0 +1,137 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.rocketmq.connect.jdbc;
+
+import io.openmessaging.KeyValue;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+import javax.jms.Session;
+
+public class Config {
+    @SuppressWarnings("serial")
+
+    public String jdbcAddr;
+    public Integer jdbcPort;
+    public String jdbcUsername;
+    public String jdbcPassword;
+
+    public String queueName;
+
+    public static final Set<String> REQUEST_CONFIG = new HashSet<String>() {
+        {
+            add("jdbcAddr");
+            add("jdbcPort");
+            add("jdbcUsername");
+            add("jdbcPassword");
+        }
+    };
+
+//    public String destinationType;
+//
+//    public String destinationName;
+//
+//    public String messageSelector;
+//
+//    private Integer sessionAcknowledgeMode = Session.AUTO_ACKNOWLEDGE;
+//
+//    private Boolean sessionTransacted = Boolean.FALSE;
+
+    public void load(KeyValue props) {
+
+        properties2Object(props, this);
+    }
+
+    private void properties2Object(final KeyValue p, final Object object) {
+        //Java Reflection Application
+        Method[] methods = object.getClass().getMethods();
+        for (Method method : methods) {
+            String mn = method.getName();
+            if (mn.startsWith("set")) {
+                try {
+                    String tmp = mn.substring(4);
+                    String first = mn.substring(3, 4);
+
+                    String key = first.toLowerCase() + tmp;
+                    String property = p.getString(key);
+                    if (property != null) {
+                        Class<?>[] pt = method.getParameterTypes();
+                        if (pt != null && pt.length > 0) {
+                            String cn = pt[0].getSimpleName();
+                            Object arg = null;
+                            if (cn.equals("int") || cn.equals("Integer")) {
+                                arg = Integer.parseInt(property);
+                            } else if (cn.equals("long") || cn.equals("Long")) {
+                                arg = Long.parseLong(property);
+                            } else if (cn.equals("double") || cn.equals("Double")) {
+                                arg = Double.parseDouble(property);
+                            } else if (cn.equals("boolean") || cn.equals("Boolean")) {
+                                arg = Boolean.parseBoolean(property);
+                            } else if (cn.equals("float") || cn.equals("Float")) {
+                                arg = Float.parseFloat(property);
+                            } else if (cn.equals("String")) {
+                                arg = property;
+                            } else {
+                                continue;
+                            }
+                            method.invoke(object, arg);
+
+                        }
+                    }
+                } catch (Throwable ignored) {
+                }
+            }
+        }
+    }
+
+    public String getJdbcAddr() {
+        return jdbcAddr;
+    }
+
+    public void setJdbcAddr(String JdbcAddr) {
+        this.jdbcAddr = jdbcAddr;
+    }
+
+    public int getJdbcPort() {
+        return jdbcPort;
+    }
+
+    public void setJdbcPort(Integer jdbcPort) {
+        this.jdbcPort = jdbcPort;
+    }
+
+    public String getJdbcUsername() {
+        return jdbcUsername;
+    }
+
+    public void setJdbcUsername(String jdbcUsername) {
+        this.jdbcUsername = jdbcUsername;
+    }
+
+    public String getJdbcPassword() {
+        return jdbcPassword;
+    }
+
+    public void setJdbcPassword(String jdbcPassword) {
+        this.jdbcPassword = jdbcPassword;
+    }
+
+
+
+
+}
