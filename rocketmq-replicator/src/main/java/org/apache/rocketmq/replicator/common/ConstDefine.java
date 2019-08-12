aff1ae0555769bba43284d6f20104a80c0f12f7b
chuenfaiy <chyang_10@163.com>
2019-08-12 10:39:23 +0800

    [Update]change the package name from  to

--- rocketmq-replicator/src/main/java/org/apache/rocketmq/replicator/common/ConstDefine.java
+++ rocketmq-replicator/src/main/java/org/apache/rocketmq/replicator/common/ConstDefine.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.rocketmq.connector.common;
+package org.apache.rocketmq.replicator.common;
 
 public class ConstDefine {
 
