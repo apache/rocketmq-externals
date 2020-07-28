f8f03aa8b5f3b8599c92cfe455370f1ddaf4f1e1
有悔 <youhui.zyf@alibaba-inc.com>
2020-07-28 16:35:17 +0800

    bug(console) add @EnableScheduling to enable cronjobs

--- rocketmq-console/src/main/java/org/apache/rocketmq/console/App.java
+++ rocketmq-console/src/main/java/org/apache/rocketmq/console/App.java
@@ -19,8 +19,10 @@ package org.apache.rocketmq.console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ServletComponentScan
 public class App {
 
