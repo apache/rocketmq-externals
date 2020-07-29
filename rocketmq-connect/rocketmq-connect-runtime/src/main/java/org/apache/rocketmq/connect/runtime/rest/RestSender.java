6f7a7592b3299462d781ff526c4b71b43fd7b4ee
Dreaouth <jliu_1996@163.com>
2020-07-29 19:04:46 +0800

    Fix checkstyle

--- rocketmq-connect/rocketmq-connect-runtime/src/main/java/org/apache/rocketmq/connect/runtime/rest/RestSender.java
+++ rocketmq-connect/rocketmq-connect-runtime/src/main/java/org/apache/rocketmq/connect/runtime/rest/RestSender.java
@@ -1,26 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.apache.rocketmq.connect.runtime.rest;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.apache.rocketmq.connect.runtime.common.LoggerName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URLEncoder;
 
 public class RestSender {
-    public String sendHttpRequest(String baseUrl, String configs){
+    private static final Logger log = LoggerFactory.getLogger(LoggerName.ROCKETMQ_RUNTIME);
+
+    public String sendHttpRequest(String baseUrl, String configs) {
+
         try {
             CloseableHttpClient client = null;
             CloseableHttpResponse response = null;
             try {
-                String encoded_configs = URLEncoder.encode(configs,"utf-8");
-                HttpGet httpGet = new HttpGet(baseUrl + encoded_configs);
+                String encodedConfigs = URLEncoder.encode(configs, "utf-8");
+                HttpGet httpGet = new HttpGet(baseUrl + encodedConfigs);
                 client = HttpClients.createDefault();
                 response = client.execute(httpGet);
                 HttpEntity entity = response.getEntity();
                 String result = EntityUtils.toString(entity);
-                System.out.println(result);
+                log.info(result);
                 return result;
             } finally {
                 if (response != null) {
