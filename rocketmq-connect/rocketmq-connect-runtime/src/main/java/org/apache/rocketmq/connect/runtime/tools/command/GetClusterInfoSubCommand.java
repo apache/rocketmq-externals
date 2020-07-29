70fb7c65a9ec38b8ff1588b60210edc099114349
Dreaouth <jliu_1996@163.com>
2020-07-29 18:27:36 +0800

    Add CLI

---
+++ rocketmq-connect/rocketmq-connect-runtime/src/main/java/org/apache/rocketmq/connect/runtime/tools/command/GetClusterInfoSubCommand.java
@@ -0,0 +1,35 @@
+package org.apache.rocketmq.connect.runtime.tools.command;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+import org.apache.rocketmq.connect.runtime.rest.RestSender;
+import org.apache.rocketmq.tools.command.SubCommandException;
+
+public class GetClusterInfoSubCommand implements SubCommand {
+    @Override
+    public String commandName() {
+        return "getClusterInfo";
+    }
+
+    @Override
+    public String commandDesc() {
+        return "Get cluster information";
+    }
+
+    @Override
+    public Options buildCommandlineOptions(Options options) {
+        return options;
+    }
+
+    @Override
+    public void execute(CommandLine commandLine, Options options) throws SubCommandException {
+        try {
+            String url = "http://localhost:8081/" + commandName();
+            System.out.println("Send request to " + url);
+            String result = new RestSender().sendHttpRequest(url, "");
+            System.out.println(result);
+        }catch (Exception e){
+            throw new SubCommandException(this.getClass().getSimpleName() + " command failed", e);
+        }
+    }
+}
