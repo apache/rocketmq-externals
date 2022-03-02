394cc20fe0a821c0bad9dcb12e3b98f244a99aea
zhoubo <877036922@qq.com>
2022-03-02 15:43:47 +0800

    delete rocketmq connect & connector dir. use new connect repository (#875)

--- rocketmq-connect-activemq/src/main/java/org/apache/rocketmq/connect/activemq/ErrorCode.java
+++
@@ -1,8 +0,0 @@
-package org.apache.rocketmq.connect.activemq;
-
-public class ErrorCode {
-
-    public static final int START_ERROR_CODE = 10001;
-
-    public static final int STOP_ERROR_CODE = 10002;
-}
--- rocketmq-connect-rabbitmq/src/main/java/org/apache/rocketmq/connect/rabbitmq/ErrorCode.java
+++
@@ -1,8 +0,0 @@
-package org.apache.rocketmq.connect.rabbitmq;
-
-public class ErrorCode {
-
-    public static final int START_ERROR_CODE = 10001;
-
-    public static final int STOP_ERROR_CODE = 10002;
-}
