728facee5d6f4bd7b6063a4f2056d947a6c8d7ad
zfj <fujian.zfj@alibaba-inc.com>
2023-08-09 19:36:14 +0800

    add license notice

--- rocketmq-rocksdb/remove_consumequeue_compactionfilterjni.cc
+++ rocketmq-rocksdb/remove_consumequeue_compactionfilterjni.cc
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include <jni.h>
 
 #include "include/org_rocksdb_RemoveConsumeQueueCompactionFilter.h"
